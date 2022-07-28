c69066586f023f030fd9cd8dbb46da7f7b08238d
JonatasJordao <jonatashjoliveira@gmail.com>
2022-07-28 14:53:32 -0300

    2 files modified

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,11 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  public listaPokemon = [];
 
-  private url= 'https://pokeapi.co/api/v2/';
+  private url = 'https://pokeapi.co/api/v2/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  buscarTodosPokemon() {
+    return this.http.get(this.url);
+  }
 }
